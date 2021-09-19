16be02e2fc3f76ba7338a479575a1a3b43ceb1d1
Logan Colborne <Logan.Colborne@gmail.com>
2021-09-19 18:52:41 -0400

    Crafting System

---
+++ Assets/Scripts/Item Types/Craftable.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Text;
+
+public class Craftable : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public CraftingSystem craftingSystem;
+    public CraftingRecipe recipe;
+    public int value;
+    public InventoryItem item;
+    public int amount;
+    public float timer = 0f;
+    public float craftingTime;
+    bool clicked;
+    public Image fill;
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            clicked = true;
+            craftingSystem.iteration = value;
+        }  
+    }    
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            timer = 0f;
+            clicked = false;
+        }  
+    }     
+
+    private void  Update() 
+    {
+        if(clicked && GameManager.Instance.CraftingCheck(GetComponent<CraftingRecipe>().items, GetComponent<CraftingRecipe>().amountRequired))
+            timer += 1f * Time.deltaTime;
+        
+        fill.fillAmount = timer / craftingTime;
+    } 
+}
---
+++ Assets/Scripts/Managers/CraftingSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingSystem : MonoBehaviour
+{
+    [SerializeField] GameObject CraftingWindow;
+    [SerializeField] Craftable[] Craftables;
+    public int iteration = 0;
+
+    private void Update() 
+    {
+        if (Craftables[iteration].timer > Craftables[iteration].craftingTime && 
+            GameManager.Instance.CraftingCheck(Craftables[iteration].GetComponent<CraftingRecipe>().items, Craftables[iteration].GetComponent<CraftingRecipe>().amountRequired))
+        {
+            GameManager.Instance.Craft(Craftables[iteration].GetComponent<CraftingRecipe>().items, Craftables[iteration].GetComponent<CraftingRecipe>().amountRequired);
+            GameManager.Instance.PickUpItem(Craftables[iteration].item.itemID, Craftables[iteration].amount);
+            Craftables[iteration].timer = 0f;
+            
+        }     
+    }
+}
