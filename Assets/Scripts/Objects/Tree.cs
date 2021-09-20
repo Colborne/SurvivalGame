9e081ca9fbe125be708b76180e4fb3ad5cdf2a82
Logan Colborne <Logan.Colborne@gmail.com>
2021-09-20 16:13:40 -0400

    trees, moved objects, fixes

--- Assets/Scripts/Objects/Tree.cs
+++ Assets/Scripts/Objects/Tree.cs
@@ -10,24 +10,31 @@ public class Tree : ResourceObject
         Stump
     }
 
+    public enum Wood {
+        Oak,
+        Maple,
+        Mahogany
+    }
+
     [SerializeField] private Type treeType;
     [SerializeField] private Transform fxTreeDestroyed;
     [SerializeField] private Transform fxTreeLogDestroyed;
     [SerializeField] private Transform treeLog;
     [SerializeField] private Transform treeLogHalf;
     [SerializeField] private Transform treeStump;
-    [SerializeField] private string type;
+    [SerializeField] private Wood type;
 
     public float timeSinceInitialization;
     public float timer;
     ObjectStats objectStats;
     float logYPositionAboveFirstLogHalf = 0f;
+    int min, max;
     
     private void Awake() 
     {
         objectStats = GetComponent<ObjectStats>();
         int healthAmount;
-        
+        min = 2; max = 4;
         switch (treeType) {
             default:
                 case Type.Tree:     healthAmount = 30; break;
@@ -36,8 +43,9 @@ public class Tree : ResourceObject
                 case Type.Stump:    healthAmount = 50; break;
         }
 
-        if(type == "Oak")
+        if(type == Wood.Oak)
         {
+            min = 4; max = 10;
             logYPositionAboveFirstLogHalf = 7f;
             switch (treeType) {
                 default:
@@ -47,8 +55,9 @@ public class Tree : ResourceObject
                 case Type.Stump:    healthAmount = 50; break;
             }
         }
-        else if(type == "Maple")
+        else if(type == Wood.Maple)
         {
+            min = 6; max = 12;
             logYPositionAboveFirstLogHalf = 10f;
             switch (treeType) {
                 default:
@@ -56,6 +65,18 @@ public class Tree : ResourceObject
                 case Type.Log:      healthAmount = 150; break;
                 case Type.LogHalf:  healthAmount = 100; break;
                 case Type.Stump:    healthAmount = 100; break;
+            }
+        }
+        else if(type == Wood.Mahogany)
+        {
+            min = 10; max = 20;
+            logYPositionAboveFirstLogHalf = 15f;
+            switch (treeType) {
+                default:
+                case Type.Tree:     healthAmount = 300; break;
+                case Type.Log:      healthAmount = 180; break;
+                case Type.LogHalf:  healthAmount = 120; break;
+                case Type.Stump:    healthAmount = 120; break;
             }
         }
         objectStats.maxHealth = healthAmount;
@@ -75,20 +96,21 @@ public class Tree : ResourceObject
             case Type.Tree:
                 Instantiate(treeLog, transform.position + transform.up, Quaternion.Euler(Random.Range(-1.5f, +1.5f), 0, Random.Range(-1.5f, +1.5f)));
                 Instantiate(treeStump, transform.position, transform.rotation);
+                Instantiate(fxTreeDestroyed, transform.position, transform.rotation);
                 break;
             case Type.Log:
                 Instantiate(treeLogHalf, transform.position, transform.rotation);
                 Instantiate(treeLogHalf, transform.position + transform.up * logYPositionAboveFirstLogHalf, transform.rotation);
                 break;
             case Type.LogHalf:
-                for(int i = 0; i < Random.Range(4,10); i++)
+                for(int i = 0; i < Random.Range(min,max); i++)
                 {
                     var _drop = Instantiate(drop, transform.position + transform.up * logYPositionAboveFirstLogHalf * .75f, Random.rotation);
                     _drop.GetComponentInChildren<Rigidbody>().AddForce(transform.up * 20f);
                 }    
                 break;
             case Type.Stump:
-                for(int i = 0; i < Random.Range(2,4); i++)
+                for(int i = 0; i < Random.Range(Mathf.Floor(min/2),Mathf.Floor(max/2)); i++)
                 {
                     var _drop = Instantiate(drop, transform.position + transform.up * 3, Random.rotation);
                     _drop.GetComponentInChildren<Rigidbody>().AddForce(transform.up * 20f);
