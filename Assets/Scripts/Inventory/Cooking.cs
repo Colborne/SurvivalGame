f3ff6cf77a73e560e7ec001f23869ed8b6dc43d6
Logan Colborne <Logan.Colborne@gmail.com>
2021-10-26 16:26:31 -0400

    can build campfire, wood is coal

--- Assets/Scripts/Inventory/Cooking.cs
+++ Assets/Scripts/Inventory/Cooking.cs
@@ -6,8 +6,8 @@ public class Cooking : MonoBehaviour
 {
     public Recipe[] recipes;
     public GameObject fx;
-    public InventoryItem coal;
-    public int coalCount;
+    public InventoryItem wood;
+    public int woodCount;
     public bool cooking = false;
     InputManager inputManager;
     InteractableUI ui; 
@@ -24,8 +24,8 @@ public class Cooking : MonoBehaviour
     {
         if (other.gameObject == GameManager.Instance.PM.gameObject)
         {
-            CoalCheck();
-            if(coalCount > 0)
+            WoodCheck();
+            if(woodCount > 0)
                 RecipeCheck();              
         }
     }
@@ -42,7 +42,7 @@ public class Cooking : MonoBehaviour
     {
         if (other.gameObject == GameManager.Instance.PM.gameObject)
         {
-            if (coalCount > 0 && !cooking)
+            if (woodCount > 0 && !cooking)
             {
                 RecipeCheck();
                 if(iter >= 0)
@@ -55,16 +55,16 @@ public class Cooking : MonoBehaviour
                     }
                 }
                 else
-                    CoalCheck();
+                    WoodCheck();
             }
             
-            if(iter < 0 && coalCount < 10)
+            if(iter < 0 && woodCount < 10)
             {
                 if(inputManager.interactInput)
                 {
                     inputManager.interactInput = false;
-                    GameManager.Instance.CheckInventoryForItem(coal, min, true);
-                    coalCount += min;
+                    GameManager.Instance.CheckInventoryForItem(wood, min, true);
+                    woodCount += min;
                 }       
             }
 
@@ -73,7 +73,7 @@ public class Cooking : MonoBehaviour
 
     void Update()
     {
-        if(coalCount > 0)
+        if(woodCount > 0)
         {
             if(timer <= 60f)
             {
@@ -83,7 +83,7 @@ public class Cooking : MonoBehaviour
             else
             {
                 timer = 0;
-                coalCount--;
+                woodCount--;
             }
 
             if(cooking)
@@ -103,12 +103,12 @@ public class Cooking : MonoBehaviour
             fx.SetActive(false);    
     }
 
-    public void CoalCheck()
+    public void WoodCheck()
     {
-        //Coal Check
+        //Wood Check
         int minAmount = 9999;
-        if(GameManager.Instance.CheckAmount(coal) >= 1) //Check the amount for each item in that recipe
-            minAmount = GameManager.Instance.CheckAmount(coal);
+        if(GameManager.Instance.CheckAmount(wood) >= 1) //Check the amount for each item in that recipe
+            minAmount = GameManager.Instance.CheckAmount(wood);
         else
             minAmount = 9999;
              
@@ -119,10 +119,10 @@ public class Cooking : MonoBehaviour
             else if(minAmount < 0)
                 minAmount = 0;
 
-            if(10 - coalCount < minAmount)
-                minAmount = 10 - coalCount;
+            if(10 - woodCount < minAmount)
+                minAmount = 10 - woodCount;
                 
-            ui.interactableText.text = "Press 'E' to Add Coal x" + minAmount;
+            ui.interactableText.text = "Press 'E' to Add Wood x" + minAmount;
             ui.transform.GetChild(0).gameObject.SetActive(true);
             min = minAmount;
         }
@@ -145,7 +145,7 @@ public class Cooking : MonoBehaviour
  
             if(iter >= 0)
             {
-                ui.interactableText.text = "Press 'E' to Add " + recipes[i].raw.item.itemName;
+                ui.interactableText.text = "Press 'E' to Cook " + recipes[i].raw.item.itemName;
                 ui.transform.GetChild(0).gameObject.SetActive(true);
                 break;
             }
