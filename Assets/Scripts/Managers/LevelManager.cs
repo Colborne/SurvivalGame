a3a72cda9f1a6e874b0cb8715ffc17c1556519f5
Logan Colborne <Logan.Colborne@gmail.com>
2021-11-09 10:25:41 -0500

    Settings Menu and scalable UI

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -4,9 +4,16 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour
 {
     [SerializeField] GameObject _loaderCanvas;
+    [SerializeField] GameObject SettingsCanvas;
+    
     public async void LoadScene(string sceneName)
     {
         var scene = SceneManager.LoadSceneAsync(sceneName);
+    }
+
+    public void Settings()
+    {
+        SettingsCanvas.SetActive(!SettingsCanvas.active);
     }
     
     public void Loaded()
