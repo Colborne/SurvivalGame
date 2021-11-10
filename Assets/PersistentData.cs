f252d9b47d7808a5b97105baab12986387fcd53e
Logan Colborne <Logan.Colborne@gmail.com>
2021-11-10 11:06:49 -0500

    Small Improvements

---
+++ Assets/PersistentData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PersistentData
+{
+    public static int seed { get; set; }
+    public static string name { get; set; }
+}
