f0baa367c6820ba87dd8c03fdf99b55e9e1f963c
Logan Colborne <Logan.Colborne@gmail.com>
2021-11-17 14:26:10 -0500

    soundeffect groundwork

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     AudioSource audioSource;
-    // Start is called before the first frame update
+    PlayerLocomotion playerLocomotion;
     private void Awake() 
     {
         audioSource = GetComponent<AudioSource>();
+        playerLocomotion = GetComponent<PlayerLocomotion>();
     }
 
     public void PlaySound(string audio)
@@ -16,7 +17,28 @@ public class SoundManager : MonoBehaviour
         var Clip = Resources.Load(audio) as AudioClip;
         if(!audioSource.isPlaying)
         {
-            audioSource.PlayOneShot(Clip, Random.Range(.3f,1f));
+            audioSource.PlayOneShot(Clip);
         }
+    }
+
+    public void Step(AnimationEvent animationEvent)
+    {
+        var Clip = Resources.Load("Sounds/Footstep_Dirt_0" + Random.Range(0,1).ToString()) as AudioClip;
+        if(!audioSource.isPlaying && animationEvent.animatorClipInfo.weight > 0.5 && playerLocomotion.isGrounded)
+            audioSource.PlayOneShot(Clip,Random.Range(.3f,.5f));
+    }
+
+    public void Sneak(AnimationEvent animationEvent)
+    {
+        var Clip = Resources.Load("Sounds/Footstep_Dirt_0" + Random.Range(0,1).ToString()) as AudioClip;
+        if(!audioSource.isPlaying&& animationEvent.animatorClipInfo.weight > 0.5)
+            audioSource.PlayOneShot(Clip,Random.Range(.1f,.2f));
+    }
+
+    public void Jump(AnimationEvent animationEvent)
+    {
+        var Clip = Resources.Load("Sounds/Footstep_Dirt_04") as AudioClip;
+        if(!audioSource.isPlaying &&animationEvent.animatorClipInfo.weight > 0.5)
+            audioSource.PlayOneShot(Clip,Random.Range(.4f,.7f));
     }
 }
